ad6acd159799de104fddf4380d353c9aa4f0f877
GIHAA <gihansudeeparandimal@gmail.com>
2024-11-27 08:01:48 +0530

    Feat: added fe admin

--- front-end-admin/src/api/endpoints/auth.endpoint.ts
+++
@@ -1,8 +0,0 @@
-import axios from '../request-handler'
-
-const login = async (data: any) => {
-  return axios.post(`/login`, data)
-}
-
-// eslint-disable-next-line import/prefer-default-export
-export { login }
