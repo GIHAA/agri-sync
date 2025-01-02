1554c6182b58ed35ee6981a2022c98a53dd84d01
COORAYNTL <tl685998@gmail.com>
2025-01-03 03:49:30 +0530

    feat/jest.config,junit.xml

--- back-end/seed-transaction-service/jest.config.js
+++ back-end/seed-transaction-service/jest.config.js
@@ -4,4 +4,11 @@ module.exports = {
   testMatch: ["**/__tests__/**/*.test.ts"],
   verbose: true,
   clearMocks: true,
+  reporters: [
+    'default',
+    ['jest-junit', {
+      outputDirectory: './back-end/seed-transaction-service',  // Adjust as needed
+      outputName: 'junit.xml', // Ensure this file name matches what you're using
+    }]
+  ],
 };
