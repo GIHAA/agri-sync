5998639e5e41a37466e005e4cd0e978230b2faf2
GIHAA <gihansudeeparandimal@gmail.com>
2025-03-16 23:55:45 +0530

    Feat: dynamic button sizes

--- mobile/app/(root)/(screens)/home.tsx
+++ mobile/app/(root)/(screens)/home.tsx
@@ -5,6 +5,7 @@ import {
   TouchableOpacity,
   Image,
   SafeAreaView,
+  ScrollView,
 } from "react-native";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import Entypo from "@expo/vector-icons/Entypo";
@@ -21,6 +22,8 @@ const HomeScreen = () => {
   const [user, setUser] = useState<User | null>(null);
   const [openQR, setOpenQR] = useState(false);
   const [devMode, setDevMode] = useState(false);
+  const [missClickCounts, setMissClickCounts] = useState(0);
+  const [buttonSizes, setButtonSizes] = useState({});
 
   useEffect(() => {
     SecureStore.getItemAsync("user").then((storedUser) => {
@@ -44,93 +47,149 @@ const HomeScreen = () => {
     setOpenQR(!openQR);
   };
 
+  const handleMissClick = (buttonId) => {
+    setMissClickCounts((prev) => prev + 1);
+    setButtonSizes((prev) => ({
+      ...prev,
+      [buttonId]: (prev[buttonId] || 55) + 10,
+    }));
+  };
+
   return (
     <SafeAreaView className="flex-1 gap-10">
-      <View className="items-center pt-8 pb-4">
-        <Image
-          source={require("../../../assets/images/app-icon.png")}
-          className="w-[126p] h-[126px] mt-[35px]"
-          resizeMode="contain"
-        />
-      </View>
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <View className="items-center pt-8 pb-4">
+          <Image
+            source={require("../../../assets/images/app-icon.png")}
+            className="w-[126px] h-[126px] mt-[35px]"
+            resizeMode="contain"
+          />
+        </View>
 
-      <View className="absolute top-12 right-4">
-        <TouchableOpacity
-          onPress={toggleDevMode}
-          className="bg-gray-800 p-2 opacity-35 rounded-lg"
-        >
-          <Text className="text-white font-bold">{devMode ? "Dev On" : "Dev Off"}</Text>
-        </TouchableOpacity>
-      </View>
-
-      <View className="flex-1 bg-green-700 rounded-t-3xl mt-4 px-4 pt-8">
-        <View className="absolute top-[-35px] left-4 transform px-6 w-full flex justify-center">
+        <View className="absolute top-12 right-4">
           <TouchableOpacity
-            className="bg-white rounded-xl py-[20px] flex-row items-center justify-center shadow-lg gap-[11px]"
-            onPress={onQRCodePress}
+            onPress={toggleDevMode}
+            className="bg-gray-800 p-2 opacity-35 rounded-lg"
           >
-            <Image
-              source={require("@/assets/images/qr.png")}
-              className="w-8 h-8"
-            />
-            <Text className="text-gray-800 font-medium">Show QR Code </Text>
+            <Text className="text-white font-bold">
+              {devMode ? "Dev On" : "Dev Off"}
+            </Text>
           </TouchableOpacity>
         </View>
 
-        {openQR && user ? (
-          <View className="mt-[78px] items-center">
-            <View className="bg-white p-4 rounded-xl">
-              <QRCode value={user.email} size={220} color="green" />
-            </View>
+        <View className="flex-1 bg-green-700 rounded-t-3xl mt-4 px-4 pt-8">
+          <View className="absolute top-[-35px] left-4 transform px-6 w-full flex justify-center">
+            <TouchableOpacity
+              className="bg-white rounded-xl py-[20px] flex-row items-center justify-center shadow-lg gap-[11px]"
+              onPress={onQRCodePress}
+            >
+              <Image
+                source={require("@/assets/images/qr.png")}
+                className="w-8 h-8"
+              />
+              <Text className="text-gray-800 font-medium">Show QR Code </Text>
+            </TouchableOpacity>
           </View>
-        ) : (
-          <View className="mt-[28px]">
-            <View className="flex-row flex-wrap justify-between">
-              {[{
-                icon: <AntDesign name="pluscircleo" size={55} color="white" />, 
-                label: "Add Farming Data",
-                buttonId: "home-addFarmingDataButton",
-                onPress: () => router.replace("/(root)/(screens)/addFarmingData"),
-              },
-              {
-                icon: <AntDesign name="gift" size={55} color="white" />, 
-                label: "Reward Program",
-                buttonId: "home-rewardsButton",
-                onPress: () => router.replace("/(root)/(screens)/rewards"),
-              },
-              {
-                icon: <AntDesign name="setting" size={55} color="white" />, 
-                label: "User Preference",
-                buttonId: "home-userPreferenceButton",
-                onPress: () => router.replace("/(root)/(screens)/user-preferences"),
-              },
-              {
-                icon: <Entypo name="chat" size={55} color="white" />, 
-                label: "Chat",
-                buttonId: "home-chatButton",
-                onPress: () => router.replace("/(root)/(screens)/chatbot"),
-              },
-              {
-                icon: <AntDesign name="login" size={55} color="white" />, 
-                label: "Logout",
-                buttonId: "home-logoutButton",
-                onPress: () => router.replace("/(auth)/sign-in"),
-              }].map((item, index) => (
-                <View key={index} className="w-[48%] mb-[5px]">
-                  <UserInteractionWrapper
-                    buttonId={item.buttonId}
-                    devmode={devMode}
-                    missClickTrackingArea={5}
-                    actualButton={
-                      <MenuOption icon={item.icon} label={item.label} onPress={item.onPress} />
-                    }
-                  />
-                </View>
-              ))}
+
+          {openQR && user ? (
+            <View className="mt-[78px] items-center">
+              <View className="bg-white p-4 rounded-xl">
+                <QRCode value={user.email} size={220} color="green" />
+              </View>
             </View>
-          </View>
-        )}
-      </View>
+          ) : (
+            <View className="mt-[28px]">
+              <View
+                className={`flex-row flex-wrap justify-${missClickCounts >= 5 ? "center" : "between"}`}
+              >
+                {[
+                  {
+                    icon: (
+                      <AntDesign
+                        name="pluscircleo"
+                        size={buttonSizes["addFarmingData"] || 55}
+                        color="white"
+                      />
+                    ),
+                    label: "Add Farming Data",
+                    buttonId: "addFarmingData",
+                    onPress: () =>
+                      router.replace("/(root)/(screens)/addFarmingData"),
+                  },
+                  {
+                    icon: (
+                      <AntDesign
+                        name="gift"
+                        size={buttonSizes["rewards"] || 55}
+                        color="white"
+                      />
+                    ),
+                    label: "Reward Program",
+                    buttonId: "rewards",
+                    onPress: () => router.replace("/(root)/(screens)/rewards"),
+                  },
+                  {
+                    icon: (
+                      <AntDesign
+                        name="setting"
+                        size={buttonSizes["userPreference"] || 55}
+                        color="white"
+                      />
+                    ),
+                    label: "User Preference",
+                    buttonId: "userPreference",
+                    onPress: () =>
+                      router.replace("/(root)/(screens)/user-preferences"),
+                  },
+                  {
+                    icon: (
+                      <Entypo
+                        name="chat"
+                        size={buttonSizes["chat"] || 55}
+                        color="white"
+                      />
+                    ),
+                    label: "Chat",
+                    buttonId: "chat",
+                    onPress: () => router.replace("/(root)/(screens)/chatbot"),
+                  },
+                  {
+                    icon: (
+                      <AntDesign
+                        name="login"
+                        size={buttonSizes["logout"] || 55}
+                        color="white"
+                      />
+                    ),
+                    label: "Logout",
+                    buttonId: "logout",
+                    onPress: () => router.replace("/(auth)/sign-in"),
+                  },
+                ].map((item, index) => (
+                  <View
+                    key={index}
+                    className={`${missClickCounts >= 5 ? "w-full" : "w-[48%]"} mb-[5px]`}
+                  >
+                    <UserInteractionWrapper
+                      buttonId={item.buttonId}
+                      devmode={devMode}
+                      missClickTrackingArea={5}
+                      onMissClick={() => handleMissClick(item.buttonId)}
+                      actualButton={
+                        <MenuOption
+                          icon={item.icon}
+                          label={item.label}
+                          onPress={item.onPress}
+                        />
+                      }
+                    />
+                  </View>
+                ))}
+              </View>
+            </View>
+          )}
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
